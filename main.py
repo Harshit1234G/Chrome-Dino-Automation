9da468129e02c75024727d6b6b76c9442d9f6c98
Harshit <harshitkumawat849@gmail.com>
2024-03-06 12:16:33 +0530

    More Accurate & works in day and night

--- main.py
+++ main.py
@@ -2,35 +2,37 @@ import pyautogui as pg
 from PIL import ImageGrab
 from time import sleep
 
+
 def isColliding(data):
     #for cactus
-    for i in range(190, 270):
+    for i in range(220, 270):
         for j in range(430, 480):
-            if data[i, j] > 100:
+            if data[i, j] == 172:
                 return 1
     #for bird 
     for i in range(220, 270):
         for j in range(380, 405):
-            if data[i, j] > 100:
+            if data[i, j] == 172:
                 return 2
             
 
 if __name__ == "__main__":
+    print('Starting in 3 seconds switch to Chrome Dino tab.')
     sleep(3)
     pg.press('up')
     while True:
         image = ImageGrab.grab().convert('L')
         data = image.load()
 
-    #     #for cactus
-    #     for i in range(190, 270):
-    #         for j in range(430, 480):
-    #             data[i, j] = 255
+        # #for cactus
+        # for i in range(220, 270):
+        #     for j in range(430, 480):
+        #         data[i, j] = 255
 
-    #     #for bird
-    #     for i in range(220, 270):
-    #         for j in range(380, 405):
-    #             data[i, j] = 200
+        # #for bird
+        # for i in range(220, 270):
+        #     for j in range(380, 405):
+        #         data[i, j] = 200
 
         match isColliding(data):
             case 1:
